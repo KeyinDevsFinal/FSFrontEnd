5781d0b974c620614f8550b6cf8c9972af3406de
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-12-13 14:34:58 -0330

    added initial routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            init
-        </p>
-      </header>
+        <Router>
+            <Routes>
+                <Route path={"/"} element={<Navigate to={"/login"} replace />}/>
+                <Route path={"/login"} element={<Login />}/>
+                <Route path={"/home"} element={<Home />}/>
+            </Routes>
+        </Router>
     </div>
   );
 }
